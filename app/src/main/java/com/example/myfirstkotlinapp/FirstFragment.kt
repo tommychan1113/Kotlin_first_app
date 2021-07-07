4bb9fdaa493afbf2362f35c7508d54d7276bfc7e
Tommy Chan <chantommy1113@gmail.com>
2021-07-07 19:31:10 +0800

    init commit

--- app/src/main/java/com/example/myfirstkotlinapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstkotlinapp.databinding.FragmentFirstBinding
 
@@ -29,8 +32,36 @@ class FirstFragment : Fragment() {
 
     }
 
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString  = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
+        }
+
+
+        // find the toast_button by its ID and set a click listener
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            // show the Toast
+            myToast.show()
+        }
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
 
     /* binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
@@ -41,4 +72,8 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
+
+
--- app/src/main/java/com/example/myfirstkotlinapp/SecondFragment.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/SecondFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstkotlinapp.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,6 +22,7 @@ class SecondFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,13 +32,25 @@ class SecondFragment : Fragment() {
         return binding.root
 
     }
+    val args: SecondFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        val count = args.myArg
+        val countText = getString(R.string.random_heading, count)
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1)
+        }
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
     }
 
     override fun onDestroyView() {
